37aaeeaf142e6c9a0ba7c262c3b8dea753041be0
FireXBG <55113543+FireXBG@users.noreply.github.com>
2023-12-26 22:04:57 +0200

    Real time fetching

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ import {
   onAuthStateChanged,
   GoogleAuthProvider,
   signInWithPopup,
+  updateProfile,
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  serverTimestamp,
+  onSnapshot,
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 /* === Firebase Setup === */
 
@@ -23,6 +31,16 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+const collectionName = "posts";
+
+/* === UI === */
+
+/* == UI - Elements == */
+
+/* === UI === */
+
+/* == UI - Elements == */
 
 /* === UI === */
 
@@ -43,6 +61,15 @@ const createAccountButtonEl = document.getElementById("create-account-btn");
 
 const signOutButtonEl = document.getElementById("sign-out-btn");
 
+const userProfilePictureEl = document.getElementById("user-profile-picture");
+const userGreetingEl = document.getElementById("user-greeting");
+
+const moodEmojiEls = document.getElementsByClassName("mood-emoji-btn");
+const textareaEl = document.getElementById("post-input");
+const postButtonEl = document.getElementById("post-btn");
+
+const postsEl = document.getElementById("posts");
+
 /* == UI - Event Listeners == */
 
 signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle);
@@ -52,11 +79,24 @@ createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail);
 
 signOutButtonEl.addEventListener("click", authSignOut);
 
+for (let moodEmojiEl of moodEmojiEls) {
+  moodEmojiEl.addEventListener("click", selectMood);
+}
+
+postButtonEl.addEventListener("click", postButtonPressed);
+
+/* === State === */
+
+let moodState = 0;
+
 /* === Main Code === */
 
 onAuthStateChanged(auth, (user) => {
   if (user) {
     showLoggedInView();
+    showProfilePicture(userProfilePictureEl, user);
+    showUserGreeting(userGreetingEl, user);
+    fetchInRealtimeAndRenderPostsFromDB();
   } else {
     showLoggedOutView();
   }
@@ -110,7 +150,66 @@ function authSignOut() {
     });
 }
 
+/* = Functions - Firebase - Cloud Firestore = */
+
+async function addPostToDB(postBody, user) {
+  try {
+    const docRef = await addDoc(collection(db, collectionName), {
+      body: postBody,
+      uid: user.uid,
+      createdAt: serverTimestamp(),
+      mood: moodState,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
+
+function fetchInRealtimeAndRenderPostsFromDB() {
+  onSnapshot(collection(db, collectionName), (querySnapshot) => {
+    clearAll(postsEl);
+
+    querySnapshot.forEach((doc) => {
+      renderPost(postsEl, doc.data());
+    });
+  });
+}
+
 /* == Functions - UI Functions == */
+
+function renderPost(postsEl, postData) {
+  postsEl.innerHTML += `
+        <div class="post">
+            <div class="header">
+                <h3>${displayDate(postData.createdAt)}</h3>
+                <img src="assets/emojis/${postData.mood}.png">
+            </div>
+            <p>
+                ${replaceNewlinesWithBrTags(postData.body)}
+            </p>
+        </div>
+    `;
+}
+
+function replaceNewlinesWithBrTags(inputString) {
+  return inputString.replace(/\n/g, "<br>");
+}
+
+function postButtonPressed() {
+  const postBody = textareaEl.value;
+  const user = auth.currentUser;
+
+  if (postBody && moodState) {
+    addPostToDB(postBody, user);
+    clearInputField(textareaEl);
+    resetAllMoodElements(moodEmojiEls);
+  }
+}
+
+function clearAll(element) {
+  element.innerHTML = "";
+}
 
 function showLoggedOutView() {
   hideView(viewLoggedIn);
@@ -137,4 +236,101 @@ function clearInputField(field) {
 function clearAuthFields() {
   clearInputField(emailInputEl);
   clearInputField(passwordInputEl);
+}
+
+function showProfilePicture(imgElement, user) {
+  const photoURL = user.photoURL;
+
+  if (photoURL) {
+    imgElement.src = photoURL;
+  } else {
+    imgElement.src = "assets/images/default-profile-picture.jpeg";
+  }
+}
+
+function showUserGreeting(element, user) {
+  const displayName = user.displayName;
+
+  if (displayName) {
+    const userFirstName = displayName.split(" ")[0];
+
+    element.textContent = `Hey ${userFirstName}, how are you?`;
+  } else {
+    element.textContent = `Hey friend, how are you?`;
+  }
+}
+
+function displayDate(firebaseDate) {
+  if (!firebaseDate) {
+    return "Date processing...";
+  }
+  const date = firebaseDate.toDate();
+
+  const day = date.getDate();
+  const year = date.getFullYear();
+
+  const monthNames = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const month = monthNames[date.getMonth()];
+
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  hours = hours < 10 ? "0" + hours : hours;
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+
+  return `${day} ${month} ${year} - ${hours}:${minutes}`;
+}
+
+/* = Functions - UI Functions - Mood = */
+
+function selectMood(event) {
+  const selectedMoodEmojiElementId = event.currentTarget.id;
+
+  changeMoodsStyleAfterSelection(selectedMoodEmojiElementId, moodEmojiEls);
+
+  const chosenMoodValue = returnMoodValueFromElementId(
+    selectedMoodEmojiElementId
+  );
+
+  moodState = chosenMoodValue;
+}
+
+function changeMoodsStyleAfterSelection(
+  selectedMoodElementId,
+  allMoodElements
+) {
+  for (let moodEmojiEl of moodEmojiEls) {
+    if (selectedMoodElementId === moodEmojiEl.id) {
+      moodEmojiEl.classList.remove("unselected-emoji");
+      moodEmojiEl.classList.add("selected-emoji");
+    } else {
+      moodEmojiEl.classList.remove("selected-emoji");
+      moodEmojiEl.classList.add("unselected-emoji");
+    }
+  }
+}
+
+function resetAllMoodElements(allMoodElements) {
+  for (let moodEmojiEl of allMoodElements) {
+    moodEmojiEl.classList.remove("selected-emoji");
+    moodEmojiEl.classList.remove("unselected-emoji");
+  }
+
+  moodState = 0;
+}
+
+function returnMoodValueFromElementId(elementId) {
+  return Number(elementId.slice(5));
 }
